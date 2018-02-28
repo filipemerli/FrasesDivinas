6711826ce01d70a3a71ff8b3e4d9188da08545b3
Filipe Merli <filipemerli@gmail.com>
2018-02-28 15:34:25 -0300

    second commit

--- Frases Divinas/Frase.swift
+++ Frases Divinas/Frase.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import Firestore
+
+protocol DocmentSerializable {
+    init?(dictionary: [String: Any])
+}
+
+struct Frase {
+    var nome: String
+    var conteudo: String
+    var dataCriada: Date
+    
+    var dictionary:[String: Any] {
+        return [
+            "nome": nome,
+            "conteudo": conteudo,
+            "dataCriada": dataCriada
+        ]
+    }
+    
+}
+
+extension Frase : DocmentSerializable {
+    init?(dictionary: [String: Any]) {
+        guard let nome = dictionary["nome"] as? String,
+            let conteudo = dictionary["conteudo"] as? String,
+            let dataCriada = dictionary["dataCriada"] as? Date else {
+                return nil
+        }
+        self.init(nome: nome, conteudo: conteudo, dataCriada: dataCriada)
+    }
+}
--- Frases Divinas/TableViewController.swift
+++ Frases Divinas/TableViewController.swift
@@ -7,30 +7,86 @@
 //
 
 import UIKit
+import Firestore
 
 class TableViewController: UITableViewController {
+    
+    var db: Firestore!
+    var fraseArray = [Frase]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        db = Firestore.firestore()
+        loadData()
+        verificarUpdates()
+    }
+    
+    func loadData() {
+        db.collection("frases").getDocuments() {
+            querySnapshot, error in
+            if let error = error {
+                print("\(error.localizedDescription)")
+            } else {
+                self.fraseArray = querySnapshot!.documents.flatMap({Frase(dictionary: $0.data())})
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func verificarUpdates() {
+        db.collection("frases").whereField("dataCriada", isGreaterThan: Date())
+            .addSnapshotListener {
+                querySnapshot, error in
+                
+                guard let snapshot = querySnapshot else {return}
+                
+                snapshot.documentChanges.forEach {
+                    diff in
+                    
+                    if diff.type == .added {
+                        self.fraseArray.append(Frase(dictionary: diff.document.data())!)
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
+                }
+                
+        }
     }
     
     @IBAction func novaFrase(_ sender: Any) {
         
-        let composeAlert = UIAlertController(title: "New Sweet", message: "Enter your name and message", preferredStyle: .alert)
+        let composeAlert = UIAlertController(title: "Nova Frase", message: "Coloque seu nome e sua mensagem", preferredStyle: .alert)
         
         composeAlert.addTextField { (textField:UITextField) in
-            textField.placeholder = "Your name"
+            textField.placeholder = "Seu nome"
         }
         
         composeAlert.addTextField { (textField:UITextField) in
-            textField.placeholder = "Your message"
+            textField.placeholder = "Sua mensagem"
         }
         
-        composeAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        composeAlert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
         
-        composeAlert.addAction(UIAlertAction(title: "Send", style: .default, handler: { (action:UIAlertAction) in
-            
-            // INTERESTING PART
+        composeAlert.addAction(UIAlertAction(title: "Enviar", style: .default, handler: { (action:UIAlertAction) in
+
+            if let nome = composeAlert.textFields?.first?.text, let conteudo = composeAlert.textFields?.last?.text {
+                
+                let novaFrase = Frase(nome: nome, conteudo: conteudo, dataCriada: Date())
+                var referencia: DocumentReference? = nil
+                referencia = self.db.collection("frases").addDocument(data: novaFrase.dictionary) {
+                    error in
+                    
+                    if let error = error {
+                        print("Erro ao adicionar documento: \(error.localizedDescription)")
+                    } else {
+                        print("Documento adicionado com o ID: \(referencia!.documentID)")
+                    }
+                }
+            }
             
         }))
         
@@ -47,13 +103,15 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return fraseArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = "Hello World"
+        let frase = fraseArray[indexPath.row]
+        cell.textLabel?.text = "\(frase.nome): \(frase.conteudo)"
+        cell.detailTextLabel?.text = "\(frase.dataCriada)"
         
         return cell
     }
